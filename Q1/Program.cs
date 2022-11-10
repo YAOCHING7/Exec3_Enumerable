288e67688f1ca302220562ee1eaaf44dee6c367b
ispan <ispan@DESKTOP-PAKSETB>
2022-11-10 17:15:46 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<int> source = Enumerable.Range(1, 10);
+			IEnumerable<string> items = source.Select(x => "Item " + x);
+	
+			foreach (var item in items)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<int> source = Enumerable.Range(10, 6);
+			IEnumerable<string> items = source.Select(x => "Item " + x);
+
+			foreach (var item in items)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
